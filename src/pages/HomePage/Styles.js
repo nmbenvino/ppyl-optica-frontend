e45614db11bcf0b0440c632c352ef3b74b4300e3
nmbenvino <nmbenvino@comunidad.unnoba.edu.ar>
2025-10-16 05:08:12 -0300

    modificados los estilos de homepage

--- src/pages/HomePage/Styles.js
+++ src/pages/HomePage/Styles.js
@@ -33,12 +33,12 @@ export const homePageStyles = {
   actionsContainer: "flex gap-2",
   button: {
     base: "py-2 px-4 rounded-md font-semibold transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 dark:focus:ring-offset-gray-900",
-    primary: "bg-blue-600 hover:bg-blue-700 text-white",
+    primary: "bg-blue-600 hover:bg-blue-800 dark:hover:bg-blue-500 text-white",
     secondary:
-      "bg-gray-200 hover:bg-gray-300 text-gray-800 dark:bg-gray-600 dark:hover:bg-gray-700 dark:text-white",
-    danger: "bg-red-600 hover:bg-red-700 text-white",
+      "bg-gray-500 hover:bg-gray-700 text-white dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-white",
+    danger: "bg-red-600 hover:bg-red-900 text-white dark:hover:bg-red-400",
     disabled:
-      "bg-gray-300 text-gray-500 cursor-not-allowed dark:bg-gray-500 dark:text-gray-300",
+      "bg-gray-300 opacity-20 text-gray-500 cursor-not-allowed dark:bg-gray-500 dark:text-gray-300",
   },
   input:
     "bg-white border border-black rounded-md p-2 w-full focus:ring-blue-500 focus:border-blue-500 text-gray-900 dark:bg-gray-700 dark:border-gray-500 dark:text-white [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none",
