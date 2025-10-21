ad87cee10ec6205a8eed622e324fadfd399e4880
nmbenvino <nmbenvino@comunidad.unnoba.edu.ar>
2025-10-21 04:02:09 -0300

    creado componente local SobresTable, interno a HomePage

---
+++ src/pages/HomePage/localComponents/SobresTable/index.jsx
@@ -0,0 +1,78 @@
+import { sobresTableStyles } from "./Styles";
+
+/**
+ * Componente que renderiza la tabla de sobres.
+ * @param {object} props - Propiedades del componente.
+ * @param {boolean} props.loading - Indica si los datos se están cargando.
+ * @param {Array} props.sobres - El array de sobres a mostrar.
+ * @param {object} props.selectedSobre - El sobre actualmente seleccionado.
+ * @param {Function} props.handleSelectSobre - Función para manejar la selección de un sobre.
+ * @param {Function} props.formatDateToDDMMYYYY - Función para formatear la fecha.
+ * @returns {JSX.Element}
+ */
+const SobresTable = ({
+  loading,
+  sobres,
+  selectedSobre,
+  handleSelectSobre,
+  formatDateToDDMMYYYY,
+}) => (
+  <div className={sobresTableStyles.tableContainer}>
+    <table className={sobresTableStyles.table}>
+      <thead className={sobresTableStyles.tableHead}>
+        <tr>
+          <th className={`${sobresTableStyles.tableHeader} w-12`}></th>
+          <th className={sobresTableStyles.tableHeader}>Cliente</th>
+          <th className={sobresTableStyles.tableHeader}>N° Sobre</th>
+          <th className={sobresTableStyles.tableHeader}>Fecha Creación</th>
+        </tr>
+      </thead>
+      <tbody>
+        {loading ? (
+          <tr>
+            <td colSpan="4" className={sobresTableStyles.tableMessage}>
+              Cargando...
+            </td>
+          </tr>
+        ) : sobres.length === 0 ? (
+          <tr>
+            <td colSpan="4" className={sobresTableStyles.tableMessage}>
+              No se encontraron sobres con los filtros aplicados.
+            </td>
+          </tr>
+        ) : (
+          sobres.map((sobre) => (
+            <tr
+              key={sobre.id_sobre}
+              className={sobresTableStyles.tableRow}
+              onClick={() =>
+                handleSelectSobre(sobre.id_sobre, sobre.sobre_number)
+              }
+            >
+              <td className={sobresTableStyles.tableCell}>
+                <input
+                  type="radio"
+                  name="selectedSobre"
+                  checked={selectedSobre.id === sobre.id_sobre}
+                  readOnly
+                  className={sobresTableStyles.radioInput}
+                />
+              </td>
+              <td
+                className={sobresTableStyles.tableCell}
+              >{`${sobre.cliente.customer_name} ${sobre.cliente.last_name}`}</td>
+              <td className={sobresTableStyles.tableCell}>
+                {sobre.sobre_number}
+              </td>
+              <td className={sobresTableStyles.tableCell}>
+                {formatDateToDDMMYYYY(sobre.sobre_date)}
+              </td>
+            </tr>
+          ))
+        )}
+      </tbody>
+    </table>
+  </div>
+);
+
+export default SobresTable;
